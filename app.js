fe993f3ff0a52815706f7d9cc381469f1011b337
NicolasPrados <nico_prados18@hotmail.com>
2023-05-03 02:04:09 -0300

    modificando resolve

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require ('path') // para unificar rutas utilizamos path -- es un pa
 
 const app = express()
 
-const publicPath = path.resolver(__dirname, './public')
+const publicPath = path.resolve(__dirname, './public')
 app.use( express.static(publicPath) ) // para decirle a express que ./public contiene archivos estaticos
 
 app.listen(3000, () => {
