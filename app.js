6334509f9ec565db2d12ff494ac25137c69a8225
richardmazo <richard.mazo.97@hotmail.com>
2023-05-11 17:16:09 -0500

    Agregando sing up en app.js

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const path = require ('path') // para unificar rutas utilizamos path -- es un paquete de node
+const express = require('express');
+const path = require ('path'); // para unificar rutas utilizamos path -- es un paquete de node
 
-const app = express()
+const app = express();
 
 const publicPath = path.resolve(__dirname, './public')
-app.use( express.static(publicPath) ) // para decirle a express que ./public contiene archivos estaticos
+app.use( express.static(publicPath)); // para decirle a express que ./public contiene archivos estaticos
 
 app.listen(3000, () => {
   console.log('Servidor en puerto 3000')
-})
+});
 
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+
+app.get('/singup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/singup.html'));
 });
