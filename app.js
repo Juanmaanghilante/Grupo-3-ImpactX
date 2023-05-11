4740c821da87d82a88e9241eedc37aa782e4010e
richardmazo <richard.mazo.97@hotmail.com>
2023-05-11 14:12:54 -0500

    singup.html

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const path = require ('path') // para unificar rutas utilizamos path -- es un paquete de node
+const express = require('express');
+const path = require ('path'); // para unificar rutas utilizamos path -- es un paquete de node
+const app = express();
 
-const app = express()
-
-const publicPath = path.resolve(__dirname, './public')
-app.use( express.static(publicPath) ) // para decirle a express que ./public contiene archivos estaticos
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath)); // para decirle a express que ./public contiene archivos estaticos
 
 app.listen(3000, () => {
   console.log('Servidor en puerto 3000')
-})
+});
 
+app.get('/singup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/singup.html'));
+});
