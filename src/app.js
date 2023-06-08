95705306ef7414f0f38492034da1695585a51ac0
allexcho <allexcho98@gmail.com>
2023-06-07 21:19:17 -0300

    port 3003

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen( 3000, () => console.log('Servidor en puerto 3000') );
+app.listen( 3003, () => console.log('Servidor en puerto 3000') );
 
 app.use (mainRouter);  
 
