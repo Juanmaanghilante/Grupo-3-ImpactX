ce0011e3b94f324105cefbce84c12c6d19e6b8eb
felipeleonn <felipeleon98@outlook.comgit config --global user.email felipeleon98@outlook.com>
2023-06-20 00:00:22 -0300

    ejs en products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const path = require("path")
 
+const rutaBase = path.resolve('./src/database/products.json')
+const productos = JSON.parse(fs.readFileSync(rutaBase));
 
 module.exports = {
 
@@ -9,7 +12,7 @@ module.exports = {
   },
 
   productsDetail: (req, res) => {
-    return res.render('products/alexProd')
+    return res.render('products/detalleProductos', { productos: productos });
   },
 
   productsCreate: (req, res) => {
@@ -17,7 +20,7 @@ module.exports = {
   },
 
   productsCreateProcess: (req, res) => {
-    
+
   },
 
   productsEdit: (req, res) => {
@@ -25,11 +28,11 @@ module.exports = {
   },
 
   productsEditProcess: (req, res) => {
-    
+
   },
 
   productsDeleteProcess: (req, res) => {
-    
+
   },
 
 
