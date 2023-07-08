48d434abc5ad70376e6548a53222b923bc7acd44
felipeleonn <felipeleon98@outlook.comgit config --global user.email felipeleon98@outlook.com>
2023-07-08 15:46:41 -0300

    agregué en header que aparezca el perfil loggeado

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -4,6 +4,8 @@ module.exports = (req, res, next) => {
   res.locals.isLogged = false;
   if(req.session && req.session.userLogged) {
     res.locals.isLogged = true;
+    res.locals.userLogged = req.session.userLogged;
+    
   }
 
   next()
