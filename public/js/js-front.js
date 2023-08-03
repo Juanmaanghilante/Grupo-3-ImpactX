b6f214115bb7d96bb91a204b3d5baa8a17209fbf
felipeleonn <felipeleon98@outlook.com>
2023-08-02 22:29:27 -0300

    .

--- public/js/js-front.js
+++ public/js/js-front.js
@@ -1,7 +1,12 @@
+console.log('holaaa!')
 const menu = document.querySelector('.svgMenu')
 const close = document.querySelector('.closeBtn')
-const nav = document.querySelector('nav')
+const nav = document.querySelector('#navPrincipal')
 
 menu.addEventListener('click', () => {
   nav.classList.add('open-nav')
+})
+
+close.addEventListener('click', () => {
+  nav.classList.remove('open-nav')
 })
