045c2e2aaae0462952739736c7370cc2d9506846
richardmazo <richard.mazo.97@hotmail.com>
2023-08-10 20:39:07 -0500

    Finalizando el formulario contactenos para clientes con base de datos

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -6,15 +6,16 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const { body } = require('express-validator');
 
 const validations = [
-    //body("nombre").notEmpty().withMessage("Tienes que escribir un nombre"),
-    //body("apellido").notEmpty().withMessage("Tienes que escribir un apellido"),
+    /*body("nombre").notEmpty().withMessage("Tienes que escribir un nombre"),
+    body("apellido").notEmpty().withMessage("Tienes que escribir un apellido"),
+    body("email").notEmpty().withMessage("You must write a email"),*/
     body("mensaje").notEmpty().withMessage("You must write a message")
 ]
 
 const validationsSendRequest = [
     body("nombre").notEmpty().withMessage("You must write a name"),
     body("apellido").notEmpty().withMessage("You must write a lastname"),
-    body("apellido").notEmpty().withMessage("You must write a email"),
+    body("email").notEmpty().withMessage("You must write a email"),
     body("mensaje").notEmpty().withMessage("You must write a message"),
     body("answer").notEmpty().withMessage("You must write an answer")
 ]
@@ -26,6 +27,7 @@ router.post('/contactenos/create', validations, mainControllerDb.createContacten
 
 //Gestionar requests
 //router.get('/request/edit', authMiddleware, mainController.request);
+//router.post('/request/:id', validationsSendRequest, mainController.sendAnswer);
 router.get('/request/edit', authMiddleware, mainControllerDb.request);
 router.post('/request/:id', validationsSendRequest, mainControllerDb.sendAnswer);
 
