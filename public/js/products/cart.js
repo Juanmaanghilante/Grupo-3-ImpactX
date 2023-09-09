c682394013a44f3145392b5825d449378e795db9
felipeleonn <felipeleon98@outlook.com>
2023-09-09 02:21:58 -0300

    css added to cart view

--- public/js/products/cart.js
+++ public/js/products/cart.js
@@ -80,7 +80,9 @@ function mostrarCarrito(productosCarrito) {
 
   
   if (productosCarrito.length == 0) {
-    carritoProd.innerHTML = `<h2>The cart is empty <a href='/productos'>¡Buy now!</a></h2>`;
+    carritoProd.innerHTML = `
+    <h2 class="emptyCartMsg">Empty Cart, <a href='/productos'>Check Our Products!</a></h2>
+    `;
     document.querySelector(".mainTotal").innerHTML = ``;
 
 
@@ -95,30 +97,32 @@ function mostrarCarrito(productosCarrito) {
       carritoProd.innerHTML += 
       `<article class="tarjetaCart">
       
-      <div class="containerInfoCart">
+        <div class="containerInfoCart">
               <img class="cartImagen" src="/img/${element.img}">
               <h2 class="name">${element.nombre}</h2>
               <p class="price">${element.precio}$</p>
               <p class="description">${element.description}</p>
-              </div>
+        </div>
               <button type="sumbit" class="botonCart" onClick=borrarElemento(${element.id})>X</button>
+
+        
       </article>`;
     });
 
 
     document.querySelector(".mainTotal").innerHTML = 
       `<div class="containerInfoCart">
-              <div class="totalBox">
-                <h2 class="name totalBox">Total: </h2>
+            <div class="totalBox">
+              <h2 h2 class="name totalBox">Total: </h2>
                 <h2 class="name totalBox">${subTotal.toLocaleString("en-US", { style: "currency", currency: "USD", })}</h2>
-              </div>
-        </div>
+            </div>
+      </div>
 
-        <button type="sumbit" class="botonCartConfirm" onClick=finalizarCompra()>  
-          Pay
-          <svg class="svgIconPay" viewBox="0 0 576 512"><path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"></path></svg>
-        </button>
-        <button type="sumbit" class="botonCartDelete" onClick=vaciarCarrito()>  <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg></button>
+      <button type="sumbit" class="botonCartConfirm" onClick=finalizarCompra()>  
+        Pay
+        <svg class="svgIconPay" viewBox="0 0 576 512"><path d="M512 80c8.8 0 16 7.2 16 16v32H48V96c0-8.8 7.2-16 16-16H512zm16 144V416c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V224H528zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm56 304c-13.3 0-24 10.7-24 24s10.7 24 24 24h48c13.3 0 24-10.7 24-24s-10.7-24-24-24H120zm128 0c-13.3 0-24 10.7-24 24s10.7 24 24 24H360c13.3 0 24-10.7 24-24s-10.7-24-24-24H248z"></path></svg>
+      </button>
+      <button type="sumbit" class="botonCartDelete" onClick=vaciarCarrito()>  <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg></button>
 `;
   }
 }
