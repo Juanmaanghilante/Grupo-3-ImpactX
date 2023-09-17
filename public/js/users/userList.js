057ce4b37bf99a6af4cc104fbdd411359724fa3c
richardmazo <richard.mazo.97@hotmail.com>
2023-09-17 18:24:31 -0500

    Actualizando delete user js

--- public/js/users/userList.js
+++ public/js/users/userList.js
@@ -1,28 +1,34 @@
-const deleteButton = document.getElementById("deleteButton");
-const ID = document.getElementById("userID");
-// muestra el modal de confirmación
-const isConfirmed = await Swal.fire({
-  title: "¿Estás seguro de querer eliminar el usuario?",
-  text: "No será posible recuperarlo después de esta acción",
-  icon: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  confirmButtonText: "Sí, eliminar",
-  cancelButtonText: "No, no deseo eliminarlo",
-});
-// si el usuario confirma la eliminación, lo elimina
-if (isConfirmed.isConfirmed) {
-  format.submit();
-  const response = await fetch(`/user/${ID}/?_method=delete`, {
-    method: "POST",
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  const deleteButton = document.getElementById("deleteButton");
+  const deleteForm = document.getElementById("deleteForm");
+  console.log("hola");
+  console.log(deleteButton);
+  console.log(deleteForm);
+  deleteButton.addEventListener("click", function () {
+    alert("Botón Eliminar clickeado");
+    // ...
   });
-  if (response.ok) {
-    Swal.fire(
-      "Eliminado",
-      "El usuario fue eliminado correctamente",
-      "success"
-    );
-  } else {
-    Swal.fire("Error", "Hubo un error al eliminar el usuario", "error");
-  }
+
+  deleteForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "Una vez eliminado, no podrás recuperar este usuario.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Sí, eliminar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Si se confirma la eliminación, envía el formulario
+        deleteForm.submit();
+      }
+    });
+  });
 }
