da968cd1fa7b46e14bb16561c1017372c25a8118
richardmazo <richard.mazo.97@hotmail.com>
2023-09-19 18:06:02 -0500

    Personalizando color en los alert

--- public/js/users/userList.js
+++ public/js/users/userList.js
@@ -19,7 +19,7 @@ function ready() {
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
+        confirmButtonColor: "#0c2b56",
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!",
       }).then((result) => {
