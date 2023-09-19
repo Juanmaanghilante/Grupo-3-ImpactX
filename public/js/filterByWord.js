e1e0bde9fbfd55e68bfe8c589f75f7cece194ede
richardmazo <richard.mazo.97@hotmail.com>
2023-09-18 19:46:24 -0500

    swal alert delete product are u sure?

--- public/js/filterByWord.js
+++ public/js/filterByWord.js
@@ -128,6 +128,25 @@ function ready() {
         duration: 1000
         }).showToast();
     }
+
+    if (event.target.classList.contains("eliminar")) {
+      event.preventDefault();
+      const closestForm = event.target.closest("form");
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          closestForm.submit();
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        }
+      });
+    }    
   });
 }
 
