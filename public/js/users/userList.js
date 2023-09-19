53c6752855abda6bc6929013f30be910d40b809d
richardmazo <richard.mazo.97@hotmail.com>
2023-09-18 19:25:08 -0500

    Agregando alert delete en usuarios

--- public/js/users/userList.js
+++ public/js/users/userList.js
@@ -1,3 +1,5 @@
+const usersContainer = document.querySelector(".mainUser");
+
 if (document.readyState == "loading") {
   document.addEventListener("DOMContentLoaded", ready);
 } else {
@@ -7,28 +9,25 @@ if (document.readyState == "loading") {
 function ready() {
   const deleteButton = document.getElementById("deleteButton");
   const deleteForm = document.getElementById("deleteForm");
-  console.log("hola");
-  console.log(deleteButton);
-  console.log(deleteForm);
-  deleteButton.addEventListener("click", function () {
-    alert("Botón Eliminar clickeado");
-    // ...
-  });
 
-  deleteForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    Swal.fire({
-      title: "¿Estás seguro?",
-      text: "Una vez eliminado, no podrás recuperar este usuario.",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Sí, eliminar",
-      cancelButtonText: "Cancelar",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Si se confirma la eliminación, envía el formulario
-        deleteForm.submit();
-      }
-    });
+  usersContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("deleteProcess")) {
+      event.preventDefault();
+      const closestForm = event.target.closest("form");
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          closestForm.submit();
+          Swal.fire("Deleted!", "Your file has been deleted.", "success");
+        }
+      });
+    }
   });
 }
