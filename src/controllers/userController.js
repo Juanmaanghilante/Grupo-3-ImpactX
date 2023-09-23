6c659c20bfb4f0f1943b242f4ac57357f140d293
felipeleonn <felipeleon98@outlook.com>
2023-09-23 01:26:00 -0300

    finished css user page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -208,7 +208,7 @@ module.exports = {
       console.log(error);
       await t.rollback();
     }
-    return res.redirect("/user/profile");
+    return res.redirect("/user/list");
   },
   userList: async (req, res) => {
     try {
