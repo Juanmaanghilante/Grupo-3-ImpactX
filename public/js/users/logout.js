d9d2ed4b63715493910e3e6dabbe60650e536a56
felipeleonn <felipeleon98@outlook.com>
2023-09-24 02:00:23 -0300

    .

--- public/js/users/logout.js
+++ public/js/users/logout.js
@@ -1,19 +1,21 @@
-var btnLogOut = document.querySelector("#logoutButton");
+// const btnLogOut = document.querySelector("#logoutButton");
 
-// Agrega un evento de clic al botón
-btnLogOut.addEventListener("click", function (e) {
-  e.preventDefault();
-  actualizarContadorCarrito();
+// // Agrega un evento de clic al botón
+// btnLogOut.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   actualizarContadorCarrito();
 
 
-  function actualizarContadorCarrito() {
-    const productosCarrito = [];
-    document.querySelector(
-      ".cartIconNumber p"
-    ).innerHTML = `${productosCarrito.length}`;
-    localStorage.setItem("carrito", JSON.stringify(productosCarrito));
-  }
+//   function actualizarContadorCarrito() {
+//     const productosCarrito = [];
+//     document.querySelector(
+//       ".cartIconNumber p"
+//     ).innerHTML = `${productosCarrito.length}`;
+//     localStorage.setItem("carrito", JSON.stringify(productosCarrito));
+//   }
     
-  window.location.href = btnLogOut.getAttribute("href");
+//   window.location.href = btnLogOut.getAttribute("href");
 
-});
+// });
+
+// NO DEJA DESLOGGEAR
